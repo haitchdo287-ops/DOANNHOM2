ee73e87ff8e32a863dcaa69758542c8768230914
Hai <haitchdo287@gmail.com>
2025-12-01 23:11:23 +0700

    Initial commit

---
+++ haitran-be/app/Http/Controllers/PhanQuyenController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ThemMoiPhanQuyenRequest;
+use App\Http\Requests\ThemMoiPhanQuyenResquest;
+use App\Models\PhanQuyen;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PhanQuyenController extends Controller
+{
+    public function getData(){
+
+        $id_chuc_nang = 29;
+
+        $data = PhanQuyen::get();
+
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+
+    public function createData(ThemMoiPhanQuyenResquest $request){
+
+        $id_chuc_nang = 30;
+
+        PhanQuyen::create([
+            'ten_quyen'         => $request->ten_quyen,
+        ]);
+
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Thêm mới tên quyền thành công!'
+        ]);
+    }
+    public function UpateData(ThemMoiPhanQuyenResquest $request)
+    {
+
+        $id_chuc_nang = 31;
+
+        $ten_quyen = PhanQuyen::where('id', $request->id)->first();
+        if($ten_quyen) {
+            $ten_quyen->update([
+                'ten_quyen'             => $request->ten_quyen,
+
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => "Cập Nhật tên quyền thành công!"
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => "Có Lỗi"
+            ]);
+        }
+    }
+    public function deleteData($id)
+    {
+
+        $id_chuc_nang = 32;
+
+        $ten_quyen = PhanQuyen::where('id', $id)->first();
+
+        if($ten_quyen) {
+            $ten_quyen->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => "Đã xóa tên quyền thành công!"
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => "Có Lỗi"
+            ]);
+        }
+    }
+
+}
+
---
+++ haitran-be/app/Providers/BroadcastServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\ServiceProvider;
+
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // Broadcast::routes();
+
+        // require base_path('routes/channels.php');
+        Broadcast::channel('App.User.{id}', function ($user, $id) {
+            return (int) $user->id === (int) $id;
+        });
+    }
+}
