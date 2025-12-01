0d1c18871a26738286b1ba2275bb5ffef1a6d2cd
Hai <haitchdo287@gmail.com>
2025-12-01 23:15:54 +0700

    Initial commit

---
+++ src/layout/components/BotRocker.vue
@@ -0,0 +1,14 @@
+<template>
+        <div class="overlay toggle-icon"></div>
+   		<footer class="page-footer">
+			<p class="mb-0">Copyright © 2024. Đây là TEAM 420.</p>
+		</footer>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
